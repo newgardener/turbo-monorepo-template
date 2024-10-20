c023750f08f315eca2cfcd9b22f02a654454810a
newgardener <jennyshin106@gmail.com>
2024-10-20 14:21:51 +0900

    chore: lint:fix

--- apps/csr/src/App.tsx
+++ apps/csr/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,16 +10,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noopener" target="_blank">
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" rel="noopener" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
